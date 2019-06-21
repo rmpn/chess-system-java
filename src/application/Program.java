69db0a1be3f244c69ad75479c67a43e338bbadb1
Raimundo M P Neto <rmpn.pinto@gmail.com>
2019-06-21 19:07:51 -0300

    Camada Chess e imprimido tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,14 +2,18 @@ package application;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
+
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 		
-		Board board = new Board(8,8);
 		
-		
+		Board board = new Board(8, 8);
+
 	}
 
 }
