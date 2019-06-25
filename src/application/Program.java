6db9d090f232621a89a22c293a216d3b879f27af
Raimundo M P Neto <rmpn.pinto@gmail.com>
2019-06-25 20:13:02 -0300

    Oi para testar

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.ChessMatch;
 
 public class Program {
@@ -11,7 +10,7 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		UI.printBoard(chessMatch.getPieces());
 		
-		
+		System.out.println("Oi");
 		Board board = new Board(8, 8);
 
 	}
